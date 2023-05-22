9c07006e1eb255f6accddd2b65534147d58873d7
Geunyeong-Kwon <124476489+Geunyeong-Kwon@users.noreply.github.com>
2023-05-22 20:16:46 +0900

    commit changes

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+
+st.title("title is the app title")
+st.markdown("this is the markdown")
+st.header("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'''
+ a + a r^1 + a r^2 + a r^3 ''')
+
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick your gender', ['Male', 'Female'])
+st.selectbox('Pick your gender', ['Male', 'Female'])
+st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number',0,50)
+                                 
+
+
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 st.sidebar.title("This is written inside the sidebar")
